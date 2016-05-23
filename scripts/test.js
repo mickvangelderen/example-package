6fb1d5b11fae5ed52413c5fee70c5c5e594ed7cc
Mick van Gelderen <mickvangelderen@gmail.com>
2016-05-23 11:23:42 +0200

    Initial commit

---
+++ scripts/test.js
@@ -0,0 +1,3 @@
+import testHello from '../src/index.test'
+
+testHello()
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import hello from './'
+import assert from 'assert'
+
+export default () => {
+	assert.strictEqual(typeof hello, 'function')
+	assert.strictEqual(hello(), 'Hello World')
+}
